238ff8cf70b053a7d2a149fc84568ad39952bb6e
Tony Russo <ynotrusso@users.noreply.github.com>
2023-05-05 11:37:44 +0700

    body parser

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const app = express()
 app.use(cors())
+app.use(bodyParser.json())
 
 async function connectToDB() {
   try {
