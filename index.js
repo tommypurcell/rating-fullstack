1a3c1fb849eb0de134727b7aa4b7da45f222decd
Tony Russo <ynotrusso@users.noreply.github.com>
2023-05-05 11:35:35 +0700

    connect to db

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 const app = express()
 app.use(cors())
 
+async function connectToDB() {
+  try {
+    await mongoose.connect('INSERT MONGODB CONNECTION STRING')
+    console.log('Connected to MongoDB')
+  } catch (err) {
+    console.log(err)
+  }
+}
+
 app.listen(4000, () => {
   console.log('Ready at http://localhost:4000')
+  connectToDB()
 })
