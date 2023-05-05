1109a171a80ea445e68a62ad92b74d01821116c9
Tony Russo <ynotrusso@users.noreply.github.com>
2023-05-05 11:37:59 +0700

    comments

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// Packages
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const app = express()
+
+// Middleware
 app.use(cors())
 app.use(bodyParser.json())
 
+// Database
 async function connectToDB() {
   try {
     await mongoose.connect('INSERT MONGODB CONNECTION STRING')
@@ -15,6 +19,11 @@ async function connectToDB() {
   }
 }
 
+// Models
+
+// Routes
+
+// Server
 app.listen(4000, () => {
   console.log('Ready at http://localhost:4000')
   connectToDB()
